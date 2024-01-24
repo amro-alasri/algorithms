351f0ec274f77d2424843792d3aface0b4c39717
Amro Alasri <98550418+amro-alasri@users.noreply.github.com>
2024-01-24 20:24:45 +0300

    add the 15. 3Sum.py

---
+++ leetcode/15. 3Sum.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result = []
+        
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue  # Skip duplicates to avoid duplicate triplets
+            
+            left, right = i + 1, len(nums) - 1
+            
+            while left < right:
+                total_sum = nums[i] + nums[left] + nums[right]
+                
+                if total_sum == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1  # Skip duplicates
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1  # Skip duplicates
+                
+                elif total_sum < 0:
+                    left += 1
+                else:
+                    right -= 1
+        
+        return result
