633b6c120271e5659dec9512395d5ff5a2707946
Amro Alasri <98550418+amro-alasri@users.noreply.github.com>
2024-01-27 23:44:09 +0300

    add the use cases for '209. Minimum Size Subarray Sum.py'

--- leetcode/209. Minimum Size Subarray Sum.py
+++ leetcode/209. Minimum Size Subarray Sum.py
@@ -19,3 +19,17 @@ class Solution(object):
                 left += 1
 
         return 0 if min_length == float('inf') else min_length
+
+# add the use cases:
+target1, nums1 = 7, [2, 3, 1, 2, 4, 3]
+s = Solution()
+result1 = s.minSubArrayLen(target1, nums1)
+print(result1)  # Output: 2
+
+target2, nums2 = 4, [1, 4, 4]
+result2 = s.minSubArrayLen(target2, nums2)
+print(result2)  # Output: 1
+
+target3, nums3 = 11, [1, 1, 1, 1, 1, 1, 1, 1]
+result3 = s.minSubArrayLen(target3, nums3)
+print(result3)  # Output: 0        
